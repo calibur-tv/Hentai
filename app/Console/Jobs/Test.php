681784b37ceacae5b0a5a20075361ac51feab6b8
falstack <icesilt@outlook.com>
2019-05-25 10:37:50 +0800

    add: test job

---
+++ app/Console/Jobs/Test.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Jobs;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'Test';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'test job';
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Log::info('test job');
+
+        return true;
+    }
+}
