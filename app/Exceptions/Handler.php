9ef9b7f4bc19a3c01d18e2fe08498af66054cf58
falstack <icesilt@outlook.com>
2019-05-30 08:06:02 +0800

    fixed: typo

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -33,7 +33,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Exception $e)
     {
         if (app()->bound('sentry') && $this->shouldReport($e))
         {
