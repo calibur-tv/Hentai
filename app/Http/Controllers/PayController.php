fb0b496442ab8b3d86aafca2a1983d6dd37dad32
falstack <icesilt@outlook.com>
2019-06-03 21:08:10 +0800

    update: creteAlipayOrder

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -9,12 +9,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Yansongda\LaravelPay\Facades\Pay;
 
 class PayController extends Controller
 {
     public function createAlipayOrder(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'type' => [
+                'required',
+                Rule::in(['app', 'web', 'wap', 'mini']),
+            ]
+        ]);
+
+        if ($validator->fails())
+        {
+            return $this->resErrParams($validator);
+        }
+
         $order = [
             'out_trade_no' => time(),
             'total_amount' => '0.01',
@@ -22,7 +36,16 @@ class PayController extends Controller
             'http_method' => 'GET'
         ];
 
-        $alipay = Pay::alipay()->web($order);
+        $type = $request->get('type');
+        if ($type === 'app') {
+            $alipay = Pay::alipay()->app($order);
+        } else if ($type === 'web') {
+            $alipay = Pay::alipay()->web($order);
+        } else if ($type === 'wap') {
+            $alipay = Pay::alipay()->wap($order);
+        } else if ($type === 'mini') {
+            $alipay = Pay::alipay()->mini($order);
+        }
 
         return $this->resOK($alipay->getTargetUrl());
     }
