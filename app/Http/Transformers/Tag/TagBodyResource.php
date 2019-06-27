ce9a4eff86f0ae18d8768593abf795697e8c44dd
falstack <icesilt@outlook.com>
2019-06-27 23:06:32 +0800

    update tag

--- app/Http/Transformers/Tag/TagBodyResource.php
+++ app/Http/Transformers/Tag/TagBodyResource.php
@@ -16,12 +16,20 @@ class TagBodyResource extends JsonResource
     {
         $extra = $this->extra()->pluck('text')->first();
         $extra = json_decode($extra, true);
+        $parentSlug = $this->parent_slug;
+        $area = array_flip(config('app.tag'));
+        $type = 'tag';
+        if (isset($area[$parentSlug]))
+        {
+            $type = $area[$parentSlug];
+        }
 
         return [
             'slug' => $this->slug,
             'name' => $this->name,
             'avatar' => $this->avatar,
-            'parent_slug' => $this->parent_slug,
+            'type' => $type,
+            'parent_slug' => $parentSlug,
             'alias' => $extra['alias'],
             'intro' => $extra['intro'] ?: '暂无简介'
         ];
