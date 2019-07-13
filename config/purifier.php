f471fc9d990a962be91f017ede6f9f19fb9b707a
falstack <icesilt@outlook.com>
2019-07-13 13:07:28 +0800

    add: mark support

--- config/purifier.php
+++ config/purifier.php
@@ -24,9 +24,9 @@ return [
     'settings'      => [
         'default' => [
             'HTML.Doctype'             => 'HTML 4.01 Transitional',
+            'HTML.Allowed'             => 'mark',
             // 'HTML.Allowed'             => 'div,b,strong,i,em,u,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
             // 'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
-            'HTML.Allowed'             => '',
             'CSS.AllowedProperties'    => '',
             'AutoFormat.AutoParagraph' => false,
             'AutoFormat.RemoveEmpty'   => false,
