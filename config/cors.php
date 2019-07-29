539e8415dc2ace21383c834f23be9db965acd527
falstack <icesilt@outlook.com>
2019-07-30 07:33:22 +0800

    update: cors

--- config/cors.php
+++ config/cors.php
@@ -11,7 +11,7 @@ return [
      |
      */
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['https://www.calibur.tv'],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
     'exposedHeaders' => ['*'],
