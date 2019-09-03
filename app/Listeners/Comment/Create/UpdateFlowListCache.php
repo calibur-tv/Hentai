25d290f2f881a0c8610b53bc47c68118fe932306
falstack <icesilt@outlook.com>
2019-09-03 17:24:24 +0800

    add: can_up

--- app/Listeners/Comment/Create/UpdateFlowListCache.php
+++ app/Listeners/Comment/Create/UpdateFlowListCache.php
@@ -34,7 +34,7 @@ class UpdateFlowListCache
             ::where('slug', $slug)
             ->first();
 
-        if (is_null($pin) || !$pin->published_at || $pin->content_type !== 1)
+        if (is_null($pin) || !$pin->published_at || $pin->content_type !== 1 || !$pin->can_up)
         {
             return;
         }
