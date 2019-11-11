0b4a5920e3e0e5d5906d201e7a6cef8ac230c060
falstack <icesilt@outlook.com>
2019-11-11 19:17:30 +0800

    save

--- app/Console/Jobs/Test.php
+++ app/Console/Jobs/Test.php
@@ -6,6 +6,8 @@ use App\Http\Repositories\PinRepository;
 use App\Http\Repositories\UserRepository;
 use App\Models\Pin;
 use App\Models\Tag;
+use App\Services\OpenSearch\Search;
+use App\Services\Spider\Query;
 use App\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -31,6 +33,58 @@ class Test extends Command
      */
     public function handle()
     {
+        $bangumiList = DB
+            ::table('bangumi_copy')
+            ->where('type', 1)
+            ->whereNull('relation_slug')
+            ->orderBy('id', 'ASC')
+            ->take(100)
+            ->get();
+
+        if (empty($bangumiList))
+        {
+            return true;
+        }
+
+        $search = new Search();
+        $query = new Query();
+        $creator = User::where('id', 2)->first();
+        $bangumiRoot = config('app.tag.bangumi');
+
+        foreach ($bangumiList as $bangumi)
+        {
+            $idolCount = DB
+                ::table('bangumi_copy')
+                ->where('type', 2)
+                ->where('relation_slug', $bangumi['source_id'])
+                ->count();
+
+            if (!$idolCount)
+            {
+                DB
+                    ::table('bangumi_copy')
+                    ->where('id', $bangumi['id'])
+                    ->update([
+                        'relation_slug' => ''
+                    ]);
+
+                continue;
+            }
+
+            $hasBangumi = $search->retrieve($bangumi['name'], 'tag');
+            if (!$hasBangumi['total'])
+            {
+                $tag = Tag::createTag($bangumi['name'], $creator, $bangumiRoot);
+            }
+        }
+        /**
+         * 1. 从 bangumi_copy 读番剧数据
+         * 2. 如果这个番剧有 idol 就继续，如果没有就标记
+         * 3. 到 search 里查有没有相似的
+         * 4. 如果有相似的，更新相似的，如果没有，就创建
+         * 5. 然后标记 bangumi_copy 和 tags 里的数据，设为已 migration
+         * 6. 把 bangumi_copy 中 idol 类别的 relation_slug 更新一下
+         */
         return true;
     }
 }
