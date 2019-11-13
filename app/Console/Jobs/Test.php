4adc169fbab22b15b998e03b2cde6bf12b5fe4aa
falstack <icesilt@outlook.com>
2019-11-13 08:57:37 +0800

    remove: cron

--- app/Console/Jobs/Test.php
+++ app/Console/Jobs/Test.php
@@ -37,28 +37,6 @@ class Test extends Command
      */
     public function handle()
     {
-        $list = Tag
-            ::where('parent_slug', config('app.tag.idol'))
-            ->where('migration_state', '<>', 5)
-            ->take(400)
-            ->get();
-
-        foreach ($list as $item)
-        {
-            IdolExtra::create([
-                'idol_slug' => $item->slug,
-                'lover_user_slug' => '',
-                'market_price' => 0,
-                'stock_price' => 0,
-                'fans_count' => 0,
-                'coin_count' => 0
-            ]);
-
-            $item->update([
-                'migration_state' => 5
-            ]);
-        }
-
         return true;
     }
 }
