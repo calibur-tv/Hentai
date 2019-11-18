d7268fefa22c37e01f1c0fbda90364aa582fda0d
falstack <icesilt@outlook.com>
2019-11-18 12:07:58 +0800

    update: docs

--- app/Models/IdolFans.php
+++ app/Models/IdolFans.php
@@ -15,8 +15,8 @@ class IdolFans extends Model
 
     protected $fillable = [
         'user_slug',
-        'idol_id',
-        'star_count',
-        'total_price',
+        'idol_slug',
+        'star_count',   // 投入的团子个数
+        'total_price',  // 每次入股时，根据当时的股价，算出得到的股票数，最终结算时，根据股票数 * 股价来计算盈亏
     ];
 }
