d761649fe3bc21d517f0289eeeb122602b22fde9
falstack <icesilt@outlook.com>
2019-11-18 19:31:50 +0800

    update: idol event

--- app/Listeners/Idol/BuyStock/UpdateIdolPatch.php
+++ app/Listeners/Idol/BuyStock/UpdateIdolPatch.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners\Idol\BuyStock;
 
+use App\Http\Modules\Counter\IdolPatchCounter;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -19,6 +20,12 @@ class UpdateIdolPatch
 
     public function handle(\App\Events\Idol\BuyStock $event)
     {
-        //
+        $slug = $event->idol->slug;
+        $idolPatchCounter = new IdolPatchCounter();
+        if (!$event->fansData)
+        {
+            $idolPatchCounter->add($slug, 'fans_count', 1);
+        }
+        $idolPatchCounter->add($slug, 'coin_count', $event->coinCount);
     }
 }
